bec021062a73e23b01ae5b399a1fec16b379a2ff
William Kunitake <wykunitake@Williams-MacBook-Pro.local>
2017-12-01 03:09:36 -0200

    Criação da página Quem Somos

---
+++ wp-content/themes/autemsolutions/page-quemsomos.php
@@ -0,0 +1,80 @@
+<?php /* Template Name: Quem Somos */ ?>
+
+<?php get_header(); ?>
+
+<div class="row" style="margin-bottom: 0">
+	<div class="col s12">
+		<div class="col m6">
+			<div style="width: 100%; height: 670px; background: linear-gradient(to bottom, rgba(0, 0, 0, 0.65) 0%, rgba(0, 0, 0, 0.65) 100%, rgba(0, 0, 0, 0.65) 100%), url(<?php bloginfo('template_url'); ?>/img/rocket-launch.png); background-size: 100% 100%;background-repeat: no-repeat;">
+			</div>
+		</div>
+		<div class="col m6 mvv">
+			<h1>MISSÃO: IMPULSIONAR SONHOS</h1>
+			<p class="justify">Queremos ajudar pessoas e empreendimentos a tornarem sustentáveis os seus objetivos e, com isso, impactar diretamente na economia e na vida das pessoas, fazendo com que cada vez mais empresas e profissionais tenham oportunidade de crescer. Acreditamos muito no poder da conexão e, portanto, buscamos fazer nosso trabalho através dela, conectando pessoas a empresas, ofertas a demandas e oportunidades a realizações.</p>
+			<h1>VISÃO</h1>
+			<p class="justify">Nos tornarmos referência – não necessariamente em tamanho ou capital – mas no exemplo. Nosso foco é trabalhar com empresas que impactem positivamente o ambiente a sua volta e nos tornarmos seus parceiros e apoiadores, para fazê-los crescer, prosperar e influenciar na economia e na vida das pessoas.</p>
+			<h1>VALORES</h1>
+			<p class="justify">
+				<ul>
+					<li>Busca constante pela perfeição nos nossos processos e na entrega aos nossos clientes</li>
+					<li>Fidelidade aos nossos compromissos, colocando-os sempre acima da lucratividade e da conveniência</li>
+					<li>Busca pela inovação, em todos os nossos setores e mentes</li>
+					<li>Ética, honestidade e dignidade em todas as nossas relações, dentro e fora da empresa</li>
+					<li>Preço justo e acessível, conjugado com um trabalho de altíssima qualidade</li>
+				</ul>
+			</p>
+		</div>
+	</div>
+</div>
+<div class="black clientes">
+	<div class="container">
+		<div class="row">
+			<div class="col s12">
+				<h1 class="white-text padding-top">Clientes</h1>
+			</div>
+			<div class="col l12 s12">
+				<div class="col l4 s12">
+					<img src="<?php bloginfo('template_url'); ?>/img/logos/1-imunova.jpg" />
+					<h3 class="white-text">Imunova</h3>
+				</div>
+				<div class="col l4 s12">
+					<img src="<?php bloginfo('template_url'); ?>/img/logos/3-dhl.jpg" />
+					<h3 class="white-text">DHL</h3>
+				</div>
+				<div class="col l4 s12">
+					<img src="<?php bloginfo('template_url'); ?>/img/logos/4-ipam.jpg" />
+					<h3 class="white-text">IPAM Amazônia</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="autem clientes">
+	<div class="container">
+		<div class="row">
+			<div class="col s12">
+				<h1 class="black-text padding-top">Parceiros</h1>
+			</div>
+			<div class="col l12 s12">
+				<div class="col s25">
+					<img src="<?php bloginfo('template_url'); ?>/img/parceiros/1-bulk.png" />
+				</div>
+				<div class="col s25">
+					<img src="<?php bloginfo('template_url'); ?>/img/parceiros/2-gebit.png" />
+				</div>
+				<div class="col s25">
+					<img src="<?php bloginfo('template_url'); ?>/img/parceiros/3-grunit.png" />
+				</div>
+				<div class="col s25">
+					<img src="<?php bloginfo('template_url'); ?>/img/parceiros/4-albaclara.png" />
+				</div>
+				<div class="col s25">
+					<img style="margin: 92px 0;" src="<?php bloginfo('template_url'); ?>/img/parceiros/5-questionpro.png" />
+				</div>				
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<?php get_footer(); ?>
